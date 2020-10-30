e1bce0d9dc7b84914762be76cb8e5e1ffa9f5e03
Oscar Bernal <oscarbernal93@gmail.com>
2020-10-30 13:03:35 -0500

    craft table modifications for new inventory

--- mods/craft_table/api.lua
+++ mods/craft_table/api.lua
@@ -1,5 +1,5 @@
 --[[
-	Mod Craft Table for Minetest
+Mod Craft Table for Minetest
 	Copyright (C) 2020 BrunoMine (https://github.com/BrunoMine)
 	
 	You should have received a copy of the GNU General Public License
@@ -25,16 +25,12 @@ end
 
 -- Formspec for craft table
 local craft_table_form = 
-	"size[8,8.3]"..
-	default.gui_bg..
-	default.gui_bg_img..
-	default.gui_slots..
-	"list[current_player;main;0,4.25;8,1;]"..
-	"list[current_player;main;0,5.5;8,3;8]"..
-	"list[current_player;craft;1.75,0.5;3,3;]"..
-	"image[4.85,1.45;1,1;gui_furnace_arrow_bg.png^[transformR270]"..
-	"list[current_player;craftpreview;5.75,1.5;1,1;]"..
-	default.get_hotbar_bg(0,4.25)
+	"size[6,7.3]"..
+	"list[current_player;main;0,4.25;6,1;]"..
+	"list[current_player;main;0,5.5;6,2;6]"..
+	"list[current_player;craft;0.7,0.5;3,3;]"..
+	"image[3.8,1.45;1,1;gui_furnace_arrow_bg.png^[transformR270]"..
+	"list[current_player;craftpreview;4.7,1.5;1,1;]"
 
 -- On_rightclick callback for craft tables
 craft_table.on_rightclick = function(pos, node, player)
@@ -75,49 +71,6 @@ local check_craft_table = function(player)
 	return true
 end
 
-
-minetest.register_allow_player_inventory_action(function(player, action, inventory, inventory_info)
-
-	if action == "move" then
-		
-		local meta = player:get_meta()
-		-- Avoid put itens at extra slots from 2x2 craft grid
-		if inventory_info.to_list == "craft" 
-			and meta:get_string("craft_table:craft_table_grid") == "2x2"
-		and (
-				inventory_info.to_index == 3
-				or inventory_info.to_index == 6
-				or inventory_info.to_index == 7
-				or inventory_info.to_index == 8
-				or inventory_info.to_index == 9
-			)
-		then
-			return 0
-		end
-		
-		-- Close formspec when table is destroyed
-		if (inventory_info.to_list == "craft" or inventory_info.from_list == "craft")
-			and meta:get_string("craft_table:craft_table_grid") ~= "2x2"
-			and check_craft_table(player) == false 
-		then
-			minetest.close_formspec(player:get_player_name(), "craft_table:craft_table")
-			drop_craft(player)
-			return 0
-		end
-		
-	end
-	return 99
-end)
-
-minetest.register_craft_predict(function(itemstack, player, old_craft_grid, craft_inv)
-	if player:get_meta():get_string("craft_table:craft_table_grid") ~= "2x2"
-		and check_craft_table(player) == false 
-	then
-		return ''
-	end
-	return itemstack
-end)
-
 -- Modify default inventory
 if sfinv then
 	local override_table = {}
@@ -132,35 +85,18 @@ if sfinv then
 				image[4,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]
 				listring[current_player;main]
 				listring[current_player;craft]
-				image[0,5.2;1,1;gui_hb_bg.png]
-				image[1,5.2;1,1;gui_hb_bg.png]
-				image[2,5.2;1,1;gui_hb_bg.png]
-				image[3,5.2;1,1;gui_hb_bg.png]
-				image[4,5.2;1,1;gui_hb_bg.png]
-				image[5,5.2;1,1;gui_hb_bg.png]
-				image[6,5.2;1,1;gui_hb_bg.png]
-				image[7,5.2;1,1;gui_hb_bg.png]
 			]], true)
 		end
 	end
 	
 	override_table.on_enter = function(self, player, context)
-		player:get_meta():set_string("craft_table:craft_table_grid", "2x2")
 		drop_craft(player)
 	end
 	-- Drop crafting items if not in crafting page
 	override_table.on_leave = function(self, player, context)
 		drop_craft(player)
 	end
-	override_table.on_player_receive_fields = function(self, player, context, fields)
-		if fields.quit then
-			drop_craft(player)
-		end
-	end
 	
 	sfinv.override_page("sfinv:crafting", override_table)
-	
-	minetest.register_on_joinplayer(function(player)
-		player:get_meta():set_string("craft_table:craft_table_grid", "2x2")
-	end)
+
 end
